b7fa1eef74b06a473fc0895c3b9581a452de563a
meo84 <oopsiday@gmail.com>
2017-03-07 14:52:21 +0100

    test if user input is not a word

--- server.js
+++ server.js
@@ -4,11 +4,14 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var port = process.env.PORT || 8080
-
+app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
-app.use(express.static(__dirname + '/public'));
+var port = process.env.PORT || 8080
+app.listen(port, function() {
+  console.log('Our app is running on http://localhost:' + port);
+});
+
 
 app.get('/', function(req, res) {
   res.render('index');
@@ -16,13 +19,14 @@ app.get('/', function(req, res) {
 
 app.post('/', (req, res) => {
   let ingredient = req.body.text;
+  console.log(req.body);
+  console.log(req);
+  if(! /^[a-z]+$/.test(ingredient)) {
+    res.send('I can\'t cook with this, pick something else!');
+    return;
+  }
   let data = { 
-    text: 'https://www.bbcgoodfood.com/search/recipes?query=Vegan+' + ingredient,
+    text: 'https://www.bbcgoodfood.com/search/recipes?query=Vegan+' + ingredient
   };
   res.json(data);
 });
-
-app.listen(port, function() {
-  console.log('Our app is running on http://localhost:' + port);
-});
-
