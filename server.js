061dcc80a44419ce6f0f048307509e227e5aab4e
meo84 <oopsiday@gmail.com>
2017-03-14 15:58:16 +0100

    add bot response with random recipe based on user's chosen ingredient

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var jsdom  = require('jsdom');
+var recipes = [];
 
 var app = express();
 var bodyParser = require('body-parser');
@@ -21,34 +22,53 @@ app.get('/', function(req, res) {
 
 app.post('/', (req, res) => {
   let ingredient = req.body.text;
-  console.log(req.body);
-  console.log(req);
   if(! /^[a-z]+$/.test(ingredient)) {
     res.send('I can\'t cook with this, pick something else!');
     return;
   }
-  let data = { 
-    text: get_recipes(ingredient)
-  };
-  res.json(data);
+  request_recipes(ingredient, function(err, recipes) {
+    var random_recipe = recipes[Math.floor(Math.random() * recipes.length)];
+    let data = {
+      response_type: 'in_channel',
+      text: 'Check out this ' + ingredient + ' vegan recipe!',
+      attachments:[
+        {
+          title: random_recipe.title,
+          title_link: random_recipe.url,
+          image_url: random_recipe.picture
+        }
+      ]};
+    res.json(data);
+  });
 });
 
-function get_recipes(ingredient) {
+function request_recipes(ingredient, callback) {
   var url = 'https://www.bbcgoodfood.com/search/recipes?query=Vegan+' + ingredient;
-
+  recipes = [];
   jsdom.env({
     url: url,
     scripts: ['http://code.jquery.com/jquery.js'],
-    done: function (err, window) {
-      var $ = window.$;
-      console.log("Test recipe title");
-      $(".teaser-item__image").children("a").each(function() {
-        console.log(" -", $(this).children("img").attr("alt"));
-        console.log("url:", "https://www.bbcgoodfood.com" + $(this).attr("href"));
-        console.log("recipe_picture:", "https:" + $(this).children("img").attr("src"));
-      });
+    done: function (errors, window) {
+      get_recipes(errors, window);
+      callback(null, recipes);
     }
   });
-  return url;
+}
+
+function get_recipes(errors, window) {
+  var $ = window.$;
+  console.log("Test recipe title");
+  $(".teaser-item__image").children("a").each(function() {
+    var recipe = {
+      "title": $(this).children("img").attr("alt"),
+      "url": "https://www.bbcgoodfood.com" + $(this).attr("href"),
+      "picture": "https:" + $(this).children("img").attr("src")
+    };
+    console.log(" -", recipe["title"]);
+    console.log("url:", recipe["url"]);
+    console.log("recipe_picture:", recipe["picture"]);
+    recipes.push(recipe);
+  });
+  console.log("Recipes:", recipes);
 }
 
